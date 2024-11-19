481d5fd147e6a47abf0af1e57da88a1e0e69662c
gadisamenu <amegadbay@gmail.com>
2024-11-19 20:52:34 +0300

    setup kafka

--- data_receiver/main.go
+++ data_receiver/main.go
@@ -1,23 +1,60 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/gadisamenu/tolling/types"
 	"github.com/gorilla/websocket"
 )
 
+var kafkaTopic = "obuTopic"
+
 type DataReceiver struct {
 	msgch chan types.ObuData
 	conn  *websocket.Conn
+	prod  *kafka.Producer
 }
 
-func NewDataReceiver() *DataReceiver {
+func NewDataReceiver() (*DataReceiver, error) {
+	prod, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Delivery report handler for produced messages
+	go func() {
+		for e := range prod.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+
 	return &DataReceiver{
 		msgch: make(chan types.ObuData, 128),
+		prod:  prod,
+	}, nil
+}
+
+func (dr *DataReceiver) ProduceData(data types.ObuData) {
+	b, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println(err)
 	}
+	dr.prod.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &kafkaTopic, Partition: kafka.PartitionAny},
+		Value:          b,
+	}, nil)
+
 }
 
 func (dr *DataReceiver) wsHandler(w http.ResponseWriter, r *http.Request) {
@@ -43,17 +80,19 @@ func (dr *DataReceiver) wsReceiveLoop() {
 			continue
 		}
 
-		fmt.Printf("recieved data %s \n", data)
+		fmt.Printf("recieved data %+v \n", data)
 
-		dr.msgch <- data
+		dr.ProduceData(data)
 	}
 
 }
 
 func main() {
 
-	dr := NewDataReceiver()
+	dr, err := NewDataReceiver()
+	if err != nil {
+		log.Fatal(err)
+	}
 	http.HandleFunc("/ws", dr.wsHandler)
 	http.ListenAndServe(":30000", nil)
-
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/gadisamenu/tolling
 
 go 1.22.2
 
-require github.com/gorilla/websocket v1.5.3 // indirect
+require (
+	github.com/confluentinc/confluent-kafka-go/v2 v2.6.1 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
